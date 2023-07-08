80f06212ca5d96c5a31af4a181dc34d3225c14dc
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-08 20:14:54 +0300

    added navbar component and render follow up questions for users

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import "../src/App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  const [symptoms, setSymptoms] = useState('');
+  const [symptoms, setSymptoms] = useState("");
   const [result, setResult] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const options = {
-      method: 'POST',
-      url: 'https://symptom-checker4.p.rapidapi.com/analyze',
+      method: "POST",
+      url: "https://symptom-checker4.p.rapidapi.com/analyze",
       params: {
-        symptoms: symptoms
+        symptoms: symptoms,
       },
       headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '3bc4205b95msh053da429b4479cdp13dcd3jsn57e065230884',
-        'X-RapidAPI-Host': 'symptom-checker4.p.rapidapi.com'
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "3bc4205b95msh053da429b4479cdp13dcd3jsn57e065230884",
+        "X-RapidAPI-Host": "symptom-checker4.p.rapidapi.com",
       },
       data: {
-        symptoms: symptoms
-      }
+        symptoms: symptoms,
+      },
     };
 
     try {
@@ -33,30 +35,41 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Symptom Checker</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your symptoms:
+    <>
+      <Navbar />
+      <div className="symptom-div">
+        <h1>Symptom Checker</h1>
+        <form onSubmit={handleSubmit}>
+          <p>Enter your symptoms:</p>
           <input
             type="text"
             value={symptoms}
             onChange={(e) => setSymptoms(e.target.value)}
           />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      {result && result.potentialCauses && (
-        <div>
-          <h2>Analysis Result:</h2>
-          <ul>
-            {result.potentialCauses.map((cause, index) => (
-              <li key={index}>{cause}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+          <button type="submit">Submit</button>
+        </form>
+        {result && result.potentialCauses && (
+          <div>
+            <h2>Analysis Result:</h2>
+            <ul>
+              {result.potentialCauses.map((cause, index) => (
+                <li key={index}>{cause}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {result && result.followupQuestions && (
+          <div>
+            <h2>Follow up Questions:</h2>
+            <ul>
+              {result.followupQuestions.map((question, index) => (
+                <li key={index}>{question}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+      <nav>
+        <h4>Health Provider</h4>
+        <div className="registr-form">
+          <p>Sign up</p>
+          <p>Log in</p>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar
