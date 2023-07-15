b3b298d14ad28eeb89318c5968e9f1b6d0da3567
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-15 12:37:48 +0300

    added register and logn database

---
+++ controllers/users.js
@@ -0,0 +1,62 @@
+const { register, login } = require("../modules/users.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const _register = async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password + "", salt);
+
+  let lower_email = email.toLowerCase();
+  register(lower_email, hash)
+    .then((row) => {
+      res.json(row);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ msg: "user already exist" });
+    });
+};
+
+const _login = async (req, res) => {
+  login(req.body.email.toLowerCase())
+    .then(async (row) => {
+      console.log("row", row, "req", req.body);
+      if (row.length === 0)
+        return res.status(404).json({ msg: "email not found" });
+
+      const match = await bcrypt.compare(req.body.password, row[0].password);
+      if (!match) return res.status(400).json({ msg: "wrong password" });
+      console.log("user info", row);
+
+      const userid = row[0].id;
+      const email = row[0].email;
+
+      const accessToken = jwt.sign(
+        { userid, email },
+        process.env.TOKEN_SECRET,
+        {
+          expiresIn: "60s",
+        }
+      );
+
+      res.cookie("token", accessToken, {
+        httpOnly: true,
+        maxAge: 60 * 1000,
+      });
+
+      res.json({ token: accessToken });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ msg: err.message });
+    });
+};
+
+module.exports = {
+  _register,
+  _login,
+};
+
--- modules/users.js
+++ modules/users.js
@@ -1,7 +1,10 @@
-const { db } = require("./config/dbsql");
+const { db } = require("../config/dbsql");
+const { v4: uuidv4 } = require("uuid");
+
+const id = uuidv4();
 
 const register = (email, password) => {
-  return db("users").insert({ email, password }).returning("id");
+  return db("users").insert({email, password }).returning("id");
 };
 
 const login = (email) => {
