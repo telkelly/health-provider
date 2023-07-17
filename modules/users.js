51fd7702a371e419582b31170e70b42c65c330ec
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-17 21:38:07 +0300

    changed files

--- modules/users.js
+++ modules/users.js
@@ -1,10 +1,7 @@
 const { db } = require("../config/dbsql");
-const { v4: uuidv4 } = require("uuid");
-
-const id = uuidv4();
 
 const register = (email, password) => {
-  return db("users").insert({email, password }).returning("id");
+  return db("users").insert({ email, password }).returning("id");
 };
 
 const login = (email) => {
