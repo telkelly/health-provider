8ede949f3a0508d554f55c130fd119886f6432c5
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-18 15:50:53 +0300

    created new paths: about and login

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Registration from "./components/Registration";
+import Register from "./components/Register";
 import SymptomChecker from "./components/SymptomsChecker";
+import LogIn from "./components/LogIn";
+import About from './components/About'
 
 
 const App = () => {
-
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/register" element={<Registration />} />
-        <Route path="/checker" element={<SymptomChecker/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/checker" element={<SymptomChecker />} />
       </Routes>
     </>
   );
