eee72788b2e92ce6a677a60b164dc9579cf09dfc
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-18 15:50:00 +0300

    changed navigation using react router dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,22 @@
 import React from "react";
-//import { Link } from "react-router-dom";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = ['About', 'Symptoms checker'];
-const settings = ['Profile', 'Logout'];
+const pages = ["About", "Symptoms checker"];
+const settings = ["Profile", "Logout"];
 
-function Navbar() {
+const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -111,28 +110,38 @@ function Navbar() {
               color: "inherit",
               textDecoration: "none",
             }}
-          >
-            
-          </Typography>
+          ></Typography>
+
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
+            <Link to={"/about"}>
               <Button
-                key={page}
+                key={pages[0]}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
-                {page}
+                {pages[0]}
               </Button>
-            ))}
+            </Link>
+            <Link to={"/checker"}>
+              <Button
+                key={pages[1]}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {pages[1]}
+              </Button>
+            </Link>
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                Kelly
-              </IconButton>
-            </Tooltip>
-            <Menu
+            {/* <Tooltip title="Open settings"> */}
+              <Link to={"/register"}>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  Sign up
+                </IconButton>
+              </Link>
+            {/* </Tooltip> */}
+            {/* <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
@@ -153,15 +162,14 @@ function Navbar() {
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
-            </Menu>
+            </Menu> */}
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 export default Navbar;
-
 
 // const Navbar = () => {
 //   return (
