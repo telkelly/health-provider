806eb6d8aea470b544a057e5f162601e66df0e0d
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-20 01:01:53 +0300

    filled about with description

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,24 @@
 const About = () => {
-    return (
-        <h1>About</h1>
-    )
-}
+  return (
+    <>
+      <h1>About The Product</h1>
+      <p>
+        Symptom Checker is an intuitive tool that allows you to
+        input your symptoms, providing a detailed overview of possible
+        underlying causes. Whether you're experiencing headaches, fatigue,
+        stomach pain, or any other discomfort, SymptomChecker Pro analyzes your
+        symptoms and presents you with a range of potential conditions or
+        illnesses associated with those symptoms. It's important to note that<br/>
+        **SymptomChecker serves as a helpful guide and should not replace
+        professional medical advice. While the tool provides you with possible
+        causes, it is essential to consult with your doctor or healthcare
+        provider for a conclusive diagnosis. The guidance provided by
+        Symptom Checker is meant to support your conversations with medical
+        professionals, ensuring that you can provide them with comprehensive
+        information about your symptoms.
+      </p>
+    </>
+  );
+};
 
-export default About
+export default About;
