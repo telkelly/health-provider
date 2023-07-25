84c49d2104718632e35ed8fdd6745010f22928a6
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-25 20:38:52 +0300

    did litle changes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
       <Navbar />
       <div className="container">
         <Routes>
+          <Route path="/" element={<About />} />
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<LogIn />} />
